d96be68694b9552a1aeeddadd2b0806090f79b2c
maurya-deepak <dmaurya768@gmail.com>
2023-01-03 22:35:59 +0530

    Skeleton.js file code refactor use for loop instead of map to show multiple loaders add comment on number of loaders shown

--- src/skeletons/Skeleton.js
+++ src/skeletons/Skeleton.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { memo } from "react";
 import SkeletonProfile from "./SkeletonProfile";
 import SkeletonRepo from "./SkeletonRepo";
 
-const Skeleton = () => {
+const Skeleton = memo(() => {
+  const getSkeletonLoaders = () => {
+    const loaders = [];
+    // Show 6 Card loaders for repository card
+    for (let loader = 1; loader <= 6; loader++) {
+      loaders.push(<SkeletonRepo key={loader} />);
+    }
+    return loaders;
+  }
   return (
     <div className="main-content">
       <SkeletonProfile />
       <div className="repos">
-        {[1, 2, 3, 4, 5, 6].map((item) => (
-          <SkeletonRepo key={item} />
-        ))}
+        {getSkeletonLoaders()}
       </div>
     </div>
   );
-};
+});
 
+Skeleton.displayName = "SkeletonLoader";
 export default Skeleton;
